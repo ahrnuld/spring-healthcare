b74c498e9d277824ac3f9a3d456388abde6cb082
Mark <mark@starwave.nl>
2020-07-24 21:20:41 +0200

    Project initialized

---
+++ src/main/java/ahrnuld/spring/springhealthcare/SpringHealthcareApplication.java
@@ -0,0 +1,13 @@
+package ahrnuld.spring.springhealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHealthcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHealthcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ahrnuld/spring/springhealthcare/SpringHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package ahrnuld.spring.springhealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHealthcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
