fa4be9ce924952487019d7be75f952b6eb01c6cd
Mark <mark@starwave.nl>
2020-07-25 20:20:32 +0200

    Adding POJO classes, issue #2

---
+++ src/main/java/ahrnuld/spring/springhealthcare/model/Doctor.java
@@ -0,0 +1,4 @@
+package ahrnuld.spring.springhealthcare.model;
+
+public class Doctor extends Person {
+}
---
+++ src/main/java/ahrnuld/spring/springhealthcare/model/Owner.java
@@ -0,0 +1,4 @@
+package ahrnuld.spring.springhealthcare.model;
+
+public class Owner extends Person {
+}
