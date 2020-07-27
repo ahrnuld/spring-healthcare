e9aefc396555665f99587c264f5124edbcac2a84
Mark <mark@starwave.nl>
2020-07-27 19:37:54 +0200

    Improved request mapping urls

--- spring-healthcare-web/src/main/java/ahrnuld/springhealthcare/controllers/DoctorController.java
+++ spring-healthcare-web/src/main/java/ahrnuld/springhealthcare/controllers/DoctorController.java
@@ -3,10 +3,11 @@ package ahrnuld.springhealthcare.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/doctors")
 @Controller
 public class DoctorController {
 
-    @RequestMapping({"/doctors", "/doctors/index", "doctors/index.html"})
+    @RequestMapping({"/", "/index", "/index.html"})
     public String listDoctors(){
         return "doctors/index";
     }
