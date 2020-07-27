1e015f5968440d40f369928b30c3d421fec1dd8b
Mark <mark@starwave.nl>
2020-07-27 14:17:35 +0200

    Implemented map based services. Closes #6.

---
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/services/map/DoctorServiceMap.java
@@ -0,0 +1,33 @@
+package ahrnuld.springhealthcare.services.map;
+
+import ahrnuld.springhealthcare.model.Doctor;
+import ahrnuld.springhealthcare.services.CrudService;
+
+import java.util.Set;
+
+public class DoctorServiceMap extends AbstractMapService<Doctor, Long> implements CrudService<Doctor, Long> {
+    @Override
+    public Set<Doctor> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Doctor findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Doctor save(Doctor object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Doctor object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/services/map/PetServiceMao.java
@@ -0,0 +1,33 @@
+package ahrnuld.springhealthcare.services.map;
+
+import ahrnuld.springhealthcare.model.Pet;
+import ahrnuld.springhealthcare.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMao extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
