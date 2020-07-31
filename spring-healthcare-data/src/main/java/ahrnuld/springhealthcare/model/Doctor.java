e38d4627ef920f47a39ec25016fefa79872e3c09
Mark <mark@starwave.nl>
2020-07-31 13:01:19 +0200

    Closes #27

--- spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/model/Doctor.java
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/model/Doctor.java
@@ -1,4 +1,16 @@
 package ahrnuld.springhealthcare.model;
 
+import java.util.Set;
+
 public class Doctor extends Person {
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
+    private Set<Specialty> specialties;
 }
