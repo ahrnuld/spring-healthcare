41310f1adeea7a99a1f55428b9c021258cb058e5
Mark <mark@starwave.nl>
2020-07-31 12:54:36 +0200

    Closes #26

--- spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/model/Owner.java
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/model/Owner.java
@@ -1,4 +1,16 @@
 package ahrnuld.springhealthcare.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
