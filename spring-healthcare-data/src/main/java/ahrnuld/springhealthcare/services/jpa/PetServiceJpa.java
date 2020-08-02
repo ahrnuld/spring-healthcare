301b6c89ce7efa6401845310cff2b565c0825284
Mark <mark@starwave.nl>
2020-08-02 16:49:42 +0200

    Closes #43

---
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/services/jpa/PetServiceJpa.java
@@ -0,0 +1,50 @@
+package ahrnuld.springhealthcare.services.jpa;
+
+import ahrnuld.springhealthcare.model.Pet;
+import ahrnuld.springhealthcare.repositories.PetRepository;
+import ahrnuld.springhealthcare.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetServiceJpa implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceJpa(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
