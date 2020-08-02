03c044bc97877ed25ca98e0eab09c486c89813f7
Mark <mark@starwave.nl>
2020-08-02 09:55:31 +0200

    Closes #38

--- spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/model/Pet.java
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/model/Pet.java
@@ -1,10 +1,9 @@
 package ahrnuld.springhealthcare.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /*Pet class*/
 @Entity
@@ -20,6 +19,9 @@ public class Pet extends BaseEntity {
     private Owner owner;
     private LocalDate birthDate;
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
