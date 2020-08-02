0fe92735e3f5347c996fed2dc65b3963e2f15d68
Mark <mark@starwave.nl>
2020-08-02 16:39:26 +0200

    Closes #42

---
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/services/jpa/PetTypeServiceJpa.java
@@ -0,0 +1,49 @@
+package ahrnuld.springhealthcare.services.jpa;
+
+import ahrnuld.springhealthcare.model.PetType;
+import ahrnuld.springhealthcare.repositories.PetTypeRepository;
+import ahrnuld.springhealthcare.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetTypeServiceJpa implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceJpa(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
