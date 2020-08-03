5d4419d555208196f2b5a9fa674fd3b3c84640ed
Mark <mark@starwave.nl>
2020-08-03 11:13:06 +0200

    Closes #46

---
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/services/VisitService.java
@@ -0,0 +1,6 @@
+package ahrnuld.springhealthcare.services;
+
+import ahrnuld.springhealthcare.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/services/map/VisitServiceMap.java
@@ -0,0 +1,37 @@
+package ahrnuld.springhealthcare.services.map;
+
+import ahrnuld.springhealthcare.model.Visit;
+import ahrnuld.springhealthcare.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null || object.getPet().getOwner().getId() == null)
+        {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+}
