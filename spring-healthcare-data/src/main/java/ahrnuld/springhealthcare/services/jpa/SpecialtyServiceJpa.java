db7a391c9c9465fcb5dcf48dccb2977fb82f0b17
Mark <mark@starwave.nl>
2020-08-03 10:39:23 +0200

    Closes #44

---
+++ spring-healthcare-data/src/main/java/ahrnuld/springhealthcare/services/jpa/SpecialtyServiceJpa.java
@@ -0,0 +1,50 @@
+package ahrnuld.springhealthcare.services.jpa;
+
+import ahrnuld.springhealthcare.model.Specialty;
+import ahrnuld.springhealthcare.repositories.SpecialtyRepository;
+import ahrnuld.springhealthcare.services.SpecialtyService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialtyServiceJpa implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyServiceJpa(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
